e365a8e151e72da4a22f9476ea473b1e5e0dfe08
pradeep435 <pradeep2005ravi@gmail.com>
2024-12-12 12:25:30 +0530

    done

---
+++ add.java
@@ -0,0 +1,16 @@
+﻿import java.util.Scanner;
+class add
+{
+ public static void main(String args[])
+  {
+    Scanner myobj = new Scanner(System.in);
+    System.out.println("enter first number:");
+    int firstnumber;
+    firstnumber=myobj.nextInt();
+    System.out.println("enter second number:");
+    int secondnumber;
+    secondnumber=myobj.nextInt();
+    System.out.println("Firstnumber:"+firstnumber);
+    System.out.println("Secondnumber:"+secondnumber);
+  }
+}
